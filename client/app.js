7798938a9f6fd3db7a87ca4320dab1119d98f9cd
Salman <smku1992@gmail.com>
2014-07-10 19:28:16 -0700

    Create mangetLink Factory for Submit & add ui-router

--- client/app.js
+++ client/app.js
@@ -1,1 +1,9 @@
-var app = angular.module('Trrnts', ['Trrnts.magnetLink', 'Trrnts.services']);
+angular.module('trrnts', ['trrnts.magnetLink', 'trrnts.services', 'ui-router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'magnetlink/magnetLink.html'
+    })
+});
--- client/magnetLink/magnetLink.js
+++ client/magnetLink/magnetLink.js
@@ -4,12 +4,13 @@ angular.module('trrnts.magnetLink', [])
 .controller('magnetLink', function ($scope, mangetLink) {
   $scope.form = {'magnetLink': ''};
 
-  // submit magnetLink
   $scope.submit = function () {
-    // base check: value not null
-    
+    // base check: value not null    
     if ($scope.form.magnetLink) {
-      magnetLink.submit($scope.form.magnetLink);
+      magnetLink.submit($scope.form.magnetLink)
+      .catch(function (err) {
+        console.error(err);
+      };
     }
-  }
+  };
 });
