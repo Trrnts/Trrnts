4e8943f6cdcfb7db1690addc44b4630b65b80186
joshWyatt <joshuawyatt123@gmail.com>
2014-07-11 15:53:17 -0700

    Comment

--- workers/crawler.js
+++ workers/crawler.js
@@ -20,19 +20,23 @@ Crawler.prototype.crawl = function (infoHash, callback) {
       _.each(resp.nodes, function (node) {
 
         this.nodes[node] = _.now();
-
         //add nodes to redis set
         redis.SADD('node', node, redis.print);
 
       }, this);
+
       _.each(resp.peers, function (peer) {
         this.peers[peer] = _.now();
       }, this);
     }.bind(this));
   }, this);
+
+  //current implementation simply kicks the crawler off every 100ms. This is not sustainable
+  //and will be fixed in the future.
   setTimeout(function () {
     this.crawl(infoHash);
-  }.bind(this), 1000);
+  }.bind(this), 100);
+
   console.log('nodes.length');
   console.log(_.keys(this.nodes).length + ' nodes');
   console.log(_.keys(this.peers).length + ' peers');
