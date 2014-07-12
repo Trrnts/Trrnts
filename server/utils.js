a3132a4362d951d2340f1e942fa02b14a20e69f0
joshWyatt <joshuawyatt123@gmail.com>
2014-07-11 20:31:01 -0700

    Delete old code

--- server/utils.js
+++ server/utils.js
@@ -1,11 +1,3 @@
-//convert a magnet into an object that has all the things it might need within the application
-
-//worry about where to put it later
-
-//properties
-//methods
-//make sure to set up the prototyping correctly
-
 var _ = require('lowdash');
 var redis = require('../redis');
 
@@ -17,12 +9,3 @@ exports.Magnet = function(req, parsedMagnetURI){
   this.magnetURI = magnetURI;
   this.score = -1;
 };
-
-// var magnet = {
-//   createdAt: new Date().getTime(),
-//   name: parsedMagnetURI.name,
-//   magnetURI: magnetURI,
-//   ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-//   infoHash: parsedMagnetURI.infoHash,
-//   score: -1 // Score: Indicate that this magnet has not been crawled yet.
-// };
