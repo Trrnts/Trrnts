37a109ea021ff7f513b39b8a60914c7606a8357c
joshWyatt <joshuawyatt123@gmail.com>
2014-07-11 21:32:24 -0700

    Fix bug

--- server/utils.js
+++ server/utils.js
@@ -3,7 +3,7 @@ var redis = require('../redis.js');
 
 exports.Magnet = function(req, parsedMagnetURI){
   this.name = parsedMagnetURI.name;
-  this.ip = req.headers
+  this.ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   this.infoHash = parsedMagnetURI.infoHash;
   this.createdAt = _.now();
   this.magnetURI = req.body.magnetURI;
@@ -11,7 +11,7 @@ exports.Magnet = function(req, parsedMagnetURI){
 
   redis.hmset('magnet:' + magnet.infoHash, magnet);
   redis.zadd('magnets:top', magnet.score, magnet.infoHash);
-  redis.lpush('magnets:latest', magnet.infoHash);
+  redis.zadd('magnets:latest', magnet.infoHash, magnet.createdAt);
   redis.sadd('magnets:ip:' + magnet.ip, magnet.infoHash);
   redis.rpush('magnets:crawl', magnet.infoHash);
 };
