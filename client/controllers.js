a6bb19a90e9a05c8443b68cc141036c6a5338a51
Antonio Grimaldo <grimi94@gmail.com>
2014-07-11 17:21:35 -0700

    Add functionality to LatestMagnetLinksController

--- client/controllers.js
+++ client/controllers.js
@@ -1,12 +1,12 @@
 angular.module('trrntsApp.controllers', [])
 
-.controller('SubmitMagnetLinkController', function($scope, magnetLinksFactory) {
+.controller('SubmitMagnetLinkController', function($scope, MagnetLinksFactory) {
   $scope.magnetURI = '';
 
   $scope.submit = function () {
     // base check: value not null
     if ($scope.magnetURI) {
-      magnetLinksFactory.submit($scope.magnetURI)
+      MagnetLinksFactory.submit($scope.magnetURI)
       .catch(function (err) {
         console.error(err);
       });
@@ -14,8 +14,13 @@ angular.module('trrntsApp.controllers', [])
   };
 })
 
-.controller('LatestMagnetLinksController', function ($scope) {
+.controller('LatestMagnetLinksController', function ($scope, MagnetLinksFactory) {
   $scope.latestMagnets = [];
+  MagnetLinksFactory.getLatestMagnets(10).then(function(result){
+    $scope.latestMagnets = result.data;
+  }).catch(function(){
+    $scope.latestMagnets = ['There has been an error houston'];
+  });
 })
 
 .controller('TopMagnetLinksController', function ($scope) {
