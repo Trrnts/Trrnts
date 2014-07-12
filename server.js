e77a79e9e250c3c586f2ca103dc706e577e9d607
Alexander Gugel <alexander.gugel@gmail.com>
2014-07-11 17:40:48 -0700

    Make require statements inside server.js more consistent

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-"use strict";
-
-var app   = require('./server/main/app.js'),
-    port  = process.env.PORT || app.get('port'),
-    log   = 'Listening on ' + app.get('base url') + ':' + port;
+var app = require('./server/main/app.js'),
+    port = process.env.PORT || app.get('port'),
+    log = 'Listening on ' + app.get('base url') + ':' + port;
 
 app.listen(port);
 console.log(log);
