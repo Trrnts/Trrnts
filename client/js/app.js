3397bcc754f3996d886d18d84123c97754bbfbbf
Alexander Gugel <alexander.gugel@gmail.com>
2014-07-15 16:04:29 -0700

    Clean up JS files

--- client/js/app.js
+++ client/js/app.js
@@ -6,12 +6,12 @@ angular.module('trrntsApp', [
   // Angular prefixes magnet URIs with "unsafe:", which makes them unclickable.
   // Uncomment this line if you prefer clickable magnet links.
   // $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|magnet):/);
-    $stateProvider
-        .state('trrntsApp', {
-          template: '<ui-view></ui-view>'
-        });
-  }])
-  .run(['$state', function ($state) {
-      // This transitions to 'trrntsApp.main' where we have all the logic for nested views
-      $state.transitionTo('trrntsApp.main');
-  }]);
+  $stateProvider
+    .state('trrntsApp', {
+      template: '<ui-view></ui-view>'
+    });
+}])
+.run(['$state', function ($state) {
+    // This transitions to 'trrntsApp.main' where we have all the logic for nested views
+    $state.transitionTo('trrntsApp.main');
+}]);
