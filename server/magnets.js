895ba4df29374e08d0c0a2850c06dfb6fc5cad79
Alexander Gugel <alexander.gugel@gmail.com>
2014-07-16 11:20:33 -0700

    Implement server side logic for peer stats

--- server/magnets.js
+++ server/magnets.js
@@ -3,6 +3,48 @@ var _ = require('lodash'),
     parseMagnetURI = require('magnet-uri'),
     magnets = {};
 
+var util = {};
+
+// Converts a single infoHash/ an array of infoHashes into an array of magnet
+// objects.
+util.infoHashesToMagnets = function (infoHashes, callback) {
+  if (!Array.isArray(infoHashes)) {
+    infoHashes = [infoHashes];
+  }
+  var multi = redis.multi();
+  _.each(infoHashes, function (infoHash) {
+    multi.hgetall('magnet:' + infoHash);
+    // TODO Caching
+    multi.zrevrange(['magnet:' + infoHash + ':peers', 0, 10000, 'WITHSCORES']);
+  });
+  multi.exec(function (err, results) {
+    var magnets = [];
+
+    // Every second result is the result of a ZREVRANGE (peer data for charts).
+    _.each(_.range(0, results.length, 2), function (index) {
+      // Every second item in perrsWithScores is a score.
+      var peersWithScores = results[index+1];
+
+      results[index].peers = _.reduce(_.range(0, peersWithScores.length, 2), function (peers, index) {
+        // debugger;
+        var addr = peersWithScores[index];
+        var lastSeenAt = Math.floor((parseInt(peersWithScores[index+1])/1000)/1); // group by 1 second intervalls for testing
+        console.log(peers);
+        peers[lastSeenAt] = peers[lastSeenAt] || 0;
+        peers[lastSeenAt]++;
+        return peers;
+      }, {});
+
+      if (results[index].peers) {
+        console.log(results[index]);
+      }
+      magnets.push(results[index]);
+    });
+
+    callback(null, magnets);
+  });
+};
+
 // create('127.0.0.1', 'magnet:?xt=urn:btih:c066...1337') #=> insert magnet URI
 // into database
 magnets.create = function (ip, magnetURI, callback) {
@@ -52,21 +94,6 @@ magnets.readList = function (list, start, stop, callback) {
 };
 
 // readMagnet('chkdewyduewdg') #=> get a single magnet link
-magnets.readMagnet = function (infoHash, callback) {
-  redis.hgetall('magnet:' + infoHash, callback);
-};
-
-var util = {};
-
-util.infoHashesToMagnets = function (infoHashes, callback) {
-  if (!Array.isArray(infoHashes)) {
-    infoHashes = [infoHashes];
-  }
-  var multi = redis.multi();
-  _.each(infoHashes, function (infoHash) {
-    multi.hgetall('magnet:' + infoHash);
-  });
-  multi.exec(callback);
-};
+magnets.readMagnet = util.infoHashesToMagnets;
 
 module.exports = exports = magnets;
