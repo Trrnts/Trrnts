41a66edf168d72683506e6d6cb89ad0dd53fdbbe
joshWyatt <joshuawyatt123@gmail.com>
2014-07-16 17:01:18 -0700

    Comment crawler.js

--- workers/crawlJobQueue.js
+++ workers/crawlJobQueue.js
@@ -1,6 +1,9 @@
+//See https://github.com/learnboost/kue
 var kue = require('kue'),
     redis = require('../redis');
 
+//Instantiate a crawljob queue qith kue which will help us handle multiple crawler jobs...
+//running more than a few at a time tends to explode the computers.
 var crawlJobQueue = module.exports = exports = kue.createQueue({
   redis: {
     createClientFactory: function () {
