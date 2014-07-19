0570da09e711cd2142504315ac17a5772fa7d5a1
Salman <smku1992@gmail.com>
2014-07-19 02:17:43 -0700

    Add methods to retreive data for cities, regions, countries

--- server/locations.js
+++ server/locations.js
@@ -4,22 +4,37 @@ var locations = {},
 
 locations.getByLatAndLong = function (callback) {
   // get all lls 
-  redis.ZRANGE(['geo:ll', 0, -1, 'WITHSCORES'], function (err, results) {
-    if (err || results === null) {
-      return callback(err, results);
-    }
-    
-    var latAndLong = {};
-
-    _.each(_.range(0, results.length, 2), function(index) {
-      latAndLong[results[index]] = results[index + 1];
-    });
-    console.log("locations", latAndLong, results);
-    callback(latAndLong);
-  });
+  getData('ll', callback);
 };
 
+locations.getByCountry = function (callback) {
+  getData('countries', callback);
+};
 
+locations.getByRegion = function (callback) {
+  getData('regions', callback);
+};
+
+locations.getByCity = function (callback) {
+  getData('cities', callback);
+};
+
+var getData = function(type, callback) {
+  redis.ZRANGE(['geo:' + type, 0, -1, 'WITHSCORES'], function (err, results) {
+    if (err || results === null) {
+      return callback(err, {});
+    }
+    
+    var data = {};
+
+    _.each(_.range(0, results.length, 2), function(index) {
+      data[results[index]] = results[index + 1];
+    });
+
+    console.log("locations " + type + ":", data, results);
+    callback(data);
+  });
+};
 
 
 module.exports = locations;
