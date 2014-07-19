ef6b0f94a82d137dafce931b2f3cfbab38ca7a28
Alexander Gugel <alexander.gugel@gmail.com>
2014-07-19 15:18:47 -0700

    Connect crawler to database

--- workers/crawl.js
+++ workers/crawl.js
@@ -171,8 +171,8 @@ module.exports.init = function (callback) {
 // Example usage:
 // var crawl = require('./crawl');
 // crawl.init(function () {
-//   crawl('8CA378DBC8F62E04DF4A4A0114B66018666C17CD', function (err, results) {
-//     console.log(results);
+//   crawl('8CA378DBC8F62E04DF4A4A0114B66018666C17CD', function (err, result) {
+//     console.log(result);
 //     process.exit(1);
 //   });
 // });
