b11c42a3503a9c7a5433c65d44a3413402fa59c2
Alexander Gugel <alexander.gugel@gmail.com>
2014-07-18 18:46:25 -0700

    Remove obsolete routes

--- server/router.js
+++ server/router.js
@@ -16,21 +16,6 @@ router.post('/magnets', function (req, res) {
   });
 });
 
-// http://localhost:9000/api/nodes
-// This get request will return all of the nodes in the 'node' set of the database
-// it returns an array of strings in the format of "ipadress:port"
-router.get('/nodes', function (req, res) {
-  redis.SMEMBERS('node', function(error, result) {
-    res.send(result);
-  });
-});
-
-router.get('/peers', function (req, res) {
-  redis.SMEMBERS('peer', function(error, result) {
-    res.send(result);
-  });
-});
-
 // http://localhost:9000/api/magnets/top or /latest
 // By default the api returns the last or top 10 magnets
 // Usage: localhost:9000/api/magnets/top/40      localhost:9000/api/magnets/latest/40
