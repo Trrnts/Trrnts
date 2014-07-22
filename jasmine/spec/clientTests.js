527dbee13a9acd14728da014fa00171924134db1
joshWyatt <joshuawyatt123@gmail.com>
2014-07-21 21:12:15 -0700

    Add Jasmine for testing

---
+++ jasmine/spec/clientTests.js
@@ -0,0 +1,16 @@
+// describe("A suite", function() {
+//   beforeEach(angular.mock.module('Application', []));
+
+//   beforeEach(angular.mock.inject(function($rootScope, $controller){
+//     //create an empty scope
+//     scope = $rootScope.$new();
+//     //declare the controller and inject our empty scope
+//     $controller('MainCtrl', {$scope: scope});
+//   }));
+
+//   it("contains spec with an expectation", function() {
+
+//     angular.module('testApp', []);
+//     expect(true).toBe(true);
+//   });
+// });
