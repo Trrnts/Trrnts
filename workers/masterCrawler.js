e551fd4610f9ce55e40bd73abb442372284c6fb4
Max Mathys <threadox@gmail.com>
2015-01-04 12:50:51 +0100

    Update example
    missing ttl

--- workers/masterCrawler.js
+++ workers/masterCrawler.js
@@ -63,7 +63,7 @@ crawl.init(function () {
 
 
   // Example usage:
-  // crawl('8CA378DBC8F62E04DF4A4A0114B66018666C17CD', function (err, results) {
+  // crawl('8CA378DBC8F62E04DF4A4A0114B66018666C17CD', 20*1000, function (err, results) {
   //   console.log(results);
   //
   //   process.exit(1);
